d9fc2988cbc966655d7e2c1eab1326bdc7a00c02
Emel Öztürk <xmlozturk@gmail.com>
2023-10-12 17:39:40 +0300

    Add fonts for fontconfig

---
+++ src/font_adder.c
@@ -0,0 +1,22 @@
+/* gcc font_adder.c -o libfontadder.so -shared -lfontconfig */
+
+#include <fontconfig/fontconfig.h>
+#include <stdio.h>
+
+int fontmain(char *fontdesc) {
+
+    FcInit();
+
+    const FcChar8 *file = (const FcChar8*)fontdesc;
+    FcBool fontAddStatus = FcConfigAppFontAddFile(FcConfigGetCurrent(), file);
+
+    if(fontAddStatus == FcFalse) {
+        printf("Failed to add font: %s\n", fontdesc);
+        return 1;
+    }
+
+    FcConfigBuildFonts(FcConfigGetCurrent());
+
+    printf("Font added successfully: %s\n", fontdesc);
+    return 0;
+}
